1df66a84cc80b883b9fdb9474b6c9a4a85431623
Johnny <johnny.moltbot@gmail.com>
2026-05-11 10:56:26 +0200

    Publish MadBitcoin Planner

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='madbitcoin-planner-v6';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys()) if(k!==CACHE) await caches.delete(k); await self.clients.claim();})())});
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate' || e.request.destination==='document'){
+    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
